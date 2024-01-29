45adad523df5ccd3d89d31e58f233a031bd375cc
tohasim <toreh.simonsen@live.dk>
2024-01-29 14:53:17 +0100

    Guessing works properly

--- script.js
+++ script.js
@@ -1,6 +1,51 @@
 "use strict";
 
+let tal;
+
 window.addEventListener("load", start);
 
 function start(){
+    tal = generateRandomNumber();
+    document.querySelector("#guessingForm").addEventListener("submit", receiveGuess)
+}
+
+
+function generateRandomNumber(){
+    return 42;
+}
+
+function receiveGuess(event){
+    event.preventDefault();
+    const form = event.target;
+    const value = form.guess.valueAsNumber
+    checkGuess(value)    
+}
+
+function checkGuess(guess){
+    if(guess === tal){
+        guessIsCorrect(guess)
+    } else if(guess<tal){
+        guessIsTooLow(guess);
+    } else{
+        guessIsTooHigh(guess);
+    }
+}
+
+function guessIsCorrect(guess){
+    const html = `<li>Du gæssede ${guess} - det var korrekt :))))</li>`
+    const list = document.getElementById("guessList")
+    list.insertAdjacentHTML("beforeend", html)    
+    
+}
+function guessIsTooLow(guess){
+    const html = `<li>Du gæssede ${guess} - det var for lavt</li>`
+    const list = document.getElementById("guessList")
+    list.insertAdjacentHTML("beforeend", html)    
+
+    
+}
+function guessIsTooHigh(guess){
+    const html = `<li>Du gæssede ${guess} - det var for højt</li>`
+    const list = document.getElementById("guessList")
+    list.insertAdjacentHTML("beforeend", html)    
 }
